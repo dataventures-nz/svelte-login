7eb0209e82abbf0bac4c9558c5c59a86d753fb61
Blair Nilsson <blair.nilsson@gmail.com>
2020-06-26 16:47:13 +1200

    more cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -11,9 +11,5 @@
 
 <main>
 	<Banner></Banner>
-
-	<!-- <h1>Hello {name}!</h1> -->
-	<!-- <h2>hi ho, it is a frog here</h2>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
 </main>
 
